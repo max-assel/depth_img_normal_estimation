82d940b2218d385ef2136f51b0d6b9266f6d5b2f
Maxwell Asselmeier <asselmeier.max@gmail.com>
2026-02-25 12:35:58 -0500

    removed timing again

--- include/depth_img_normal_estimation/NormalEstimator.h
+++ include/depth_img_normal_estimation/NormalEstimator.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <chrono>
+// #include <chrono>
 #include <fstream>
 #include <string>
 
@@ -43,7 +43,7 @@ class NormalEstimator
 
     private:
 
-        void log();
+        // void log();
 
         void checkSparsity(const cv::Mat& depth_img, cv_bridge::CvImagePtr& normals_ptr);
 
@@ -75,27 +75,27 @@ class NormalEstimator
 
         PinholeCamera camera;
 
-        std::chrono::steady_clock::time_point initTime;
+        // std::chrono::steady_clock::time_point initTime;
 
-        std::chrono::steady_clock::time_point totalBegin;
-        std::chrono::steady_clock::time_point totalEnd;
-        float totalTimeTaken = 0.0f;
-        int numberOfTotalCalls = 0;
+        // std::chrono::steady_clock::time_point totalBegin;
+        // std::chrono::steady_clock::time_point totalEnd;
+        // float totalTimeTaken = 0.0f;
+        // int numberOfTotalCalls = 0;
 
-        std::chrono::steady_clock::time_point preprocessBegin;
-        std::chrono::steady_clock::time_point preprocessEnd;
-        float preprocessTimeTaken = 0.0f;
-        int numberOfPreprocessCalls = 0;
+        // std::chrono::steady_clock::time_point preprocessBegin;
+        // std::chrono::steady_clock::time_point preprocessEnd;
+        // float preprocessTimeTaken = 0.0f;
+        // int numberOfPreprocessCalls = 0;
 
-        std::chrono::steady_clock::time_point paddingBegin;
-        std::chrono::steady_clock::time_point paddingEnd;
-        float paddingTimeTaken = 0.0f;
-        int numberOfPaddingCalls = 0;
+        // std::chrono::steady_clock::time_point paddingBegin;
+        // std::chrono::steady_clock::time_point paddingEnd;
+        // float paddingTimeTaken = 0.0f;
+        // int numberOfPaddingCalls = 0;
 
-        std::chrono::steady_clock::time_point depthGradientsBegin;
-        std::chrono::steady_clock::time_point depthGradientsEnd;
-        float depthGradientsTimeTaken = 0.0f;
-        int numberOfDepthGradientsCalls = 0;
+        // std::chrono::steady_clock::time_point depthGradientsBegin;
+        // std::chrono::steady_clock::time_point depthGradientsEnd;
+        // float depthGradientsTimeTaken = 0.0f;
+        // int numberOfDepthGradientsCalls = 0;
 
         struct NormalEstimationParams
         {
